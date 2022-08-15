f8b5a295b87139bff497ff588195704147187f81
Kevin Yeh <kevinyeah@utexas.edu>
2022-08-14 20:56:49 -0400

    grid markers

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -18,7 +18,7 @@ function Works() {
           <div id="about">
             <div className="fade-in content-top">
               <div className="project-grid">
-                <div className="tile-w-6 tile-h-6">
+                <div className="tile tile-w-6 tile-h-3">
                   <ProjectHoverLink
                     href="work/2022-panasonic-make-new"
                     title="Panasonic: Make New"
@@ -26,7 +26,46 @@ function Works() {
                     <img src="/works/2022-client-brand-panasonic/make-new-wide-2.png"></img>
                   </ProjectHoverLink>
                 </div>
-                <div className="tile-w-6 tile-h-6">
+                <div className="tile tile-w-4 tile-h-4">
+                  <ProjectHoverLink href="work/2022-vartype" title="Vartype">
+                    <video autoPlay={true} loop={true}>
+                      <source
+                        src="/works/2022-tool-vartype/vartype-insta-3.mp4"
+                        type="video/mp4"
+                      />
+                    </video>
+                  </ProjectHoverLink>
+                </div>
+                <div className="tile tile-w-2 tile-h-10" style={{backgroundColor: "red"}}>
+                  
+                </div>
+                
+                <div className="tile tile-w-6 tile-h-4">
+                  <ProjectHoverLink
+                    href="work/2022-existential"
+                    title="Existential Co."
+                  >
+                    <img src="/works/2022-client-web-existential/square.png"></img>
+                  </ProjectHoverLink>
+                </div>
+                <div className="tile tile-w-4 tile-h-6">
+                  <ProjectHoverLink
+                    href="work/2022-36-days-of-type"
+                    title="36 Days of Type"
+                  >
+                    <img src="/works/2022-insta-36daysoftype/squareish.png"></img>
+                  </ProjectHoverLink>
+                </div>
+                {/* <div className="tile tile-w-6 tile-h-1">
+                  <h2>Generative Typography</h2>
+                </div> */}
+                <div className="tile tile-w-6 tile-h-3" style={{backgroundColor: "blue"}}>
+                  
+                  </div>
+                <div className="tile tile-w-2 tile-h-10" style={{backgroundColor: "blue"}}>
+                  
+                </div>
+                <div className="tile tile-w-8 tile-h-4">
                   <ProjectHoverLink
                     href="work/2019-broadway-mural-project"
                     title="Broadway Mural Project"
@@ -34,7 +73,15 @@ function Works() {
                     <img src="/works/2019-client-mural-broadway/wide-still.jpg"></img>
                   </ProjectHoverLink>
                 </div>
-                <div className="tile-w-6 tile-h-6">
+                <div className="tile tile-w-4 tile-h-4">
+                  <ProjectHoverLink
+                    href="work/2022-ampersandist"
+                    title="Typeface: Ampersandist"
+                  >
+                    <img src="/works/2022-typeface-Ampersandist/large_Futurefonts_Samples-06.png"></img>
+                  </ProjectHoverLink>
+                </div>
+                <div className="tile tile-w-6 tile-h-4">
                   <ProjectHoverLink
                     href="work/2018-hanys-harvest-branding"
                     title="Hanys Harvest Branding"
@@ -47,42 +94,9 @@ function Works() {
                       <source src="/works/2022-client-brand-panasonic/4_second_clip.mov" type="video/mp4"/>
                   </video>
                 </div> */}
-                <div className="tile-w-6 tile-h-6">
-                  <ProjectHoverLink
-                    href="work/2022-36-days-of-type"
-                    title="36 Days of Type"
-                  >
-                    <img src="/works/2022-insta-36daysoftype/squareish.png"></img>
-                  </ProjectHoverLink>
-                </div>
 
-                <div className="tile-w-6 tile-h-6">
-                  <ProjectHoverLink
-                    href="work/2022-ampersandist"
-                    title="Typeface: Ampersandist"
-                  >
-                    <img src="/works/2022-typeface-Ampersandist/large_Futurefonts_Samples-06.png"></img>
-                  </ProjectHoverLink>
-                </div>
-                <div className="tile-w-6 tile-h-6">
-                  <ProjectHoverLink
-                    href="work/2022-existential"
-                    title="Existential Co."
-                  >
-                    <img src="/works/2022-client-web-existential/square.png"></img>
-                  </ProjectHoverLink>
-                </div>
-                <div className="tile-w-6 tile-h-6">
-                  <ProjectHoverLink href="work/2022-vartype" title="Vartype">
-                    <video autoPlay={true} loop={true}>
-                      <source
-                        src="/works/2022-tool-vartype/vartype-insta-3.mp4"
-                        type="video/mp4"
-                      />
-                    </video>
-                  </ProjectHoverLink>
-                </div>
-                <div className="tile-w-6 tile-h-6">
+                
+                <div className="tile tile-w-6 tile-h-4">
                   <ProjectHoverLink href="work/zapata" title="Typeface: Zapata">
                     <img src="/works/typeface-zapata/logo.png"></img>
                   </ProjectHoverLink>
