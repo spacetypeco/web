ff8d6432c033149ce4bc81e6b2f4c2aca3c2177c
Kevin Yeh <kevinyeah@utexas.edu>
2022-08-20 15:47:24 -0400

    fix video manager

--- hooks/useVideoManager.jsx
+++ hooks/useVideoManager.jsx
@@ -1,18 +1,17 @@
 import { useEffect } from "react";
 
-var observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(function (entry) {
-      // Pause/Play the animation
-      if (entry.isIntersecting) {
-        entry.target.play();
-      } else {
-        entry.target.pause();
-      }
-    });
-  });
-
 export default function useVideoManager() {
     useEffect(() => {
+      var observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+          // Pause/Play the animation
+          if (entry.isIntersecting) {
+            entry.target.play();
+          } else {
+            entry.target.pause();
+          }
+        });
+      });
         var videos = document.querySelectorAll("video");
         videos.forEach(function (el) {
             observer.observe(el);
