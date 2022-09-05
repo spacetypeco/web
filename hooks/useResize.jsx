abb1ac54d8c7d92ebc612d49e110a252d2d61eb6
Kevin Yeh <kevinyeah@utexas.edu>
2022-09-05 17:56:12 -0400

    short-circuit logic if not crossing a breakpoint

--- hooks/useResize.jsx
+++ hooks/useResize.jsx
@@ -1,5 +1,21 @@
 import { useEffect } from "react";
 
+const BREAKPOINTS = [500, 1000, 1500];
+
+const crossedBreakpoint = (oldWidth, newWidth) => {
+    if (newWidth >= currentWidth) {
+        return false;
+    }
+
+    for (const pt of BREAKPOINTS) {
+        if (oldWidth < pt && newWidth >= pt) {
+          return true;
+        }
+      }
+  
+      return false;
+}
+
 export default function useResize(delay, callbackFn, onlyLarger=true, callOnStart=true) {
   useEffect(() => {
     const currentWidth = 0;
@@ -13,10 +29,9 @@ export default function useResize(delay, callbackFn, onlyLarger=true, callOnStar
       handler = setTimeout(() => {
         handler = null;
         const newWidth = window.innerWidth;
-        console.log({currentWidth, newWidth});
-
-        if (!onlyLarger || newWidth > currentWidth) {
-          callbackFn();
+        
+        if (!onlyLarger || crossedBreakpoint(currentWidth, newWidth)) {
+          callbackFn(currentWidth, newWidth);
         }
 
         currentWidth = newWidth;
