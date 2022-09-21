5c142eefe3f4bceb327810eb9a935d634a7c4500
Kevin Yeh <kevinyeah@utexas.edu>
2022-09-20 22:13:22 -0400

    show Chance and Destiny final piece

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -20,36 +20,38 @@ const ReactP5Wrapper = dynamic(
 require("../../util/utils.js");
 
 const GothicTitle = (txt, classes = "") => {
-  return <>
-    <div className={classes} style={{ display: "grid" }}>
-      <p
-        className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-base title title--tall"
-        style={{
-          gridArea: "1 / 1",
-          color: "var(--color-black)",
-          animationDelay: "-0.1s",
-        }}
-      >
-        {txt}
-      </p>
-      <p
-        className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-layer-1 title title--tall"
-        style={{
-          gridArea: "1 / 1",
-          color: "var(--color-title)",
-          animationDelay: "-0.1s",
-        }}
-      >
-        {txt}
-      </p>
-      <p
-        className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-layer-2 title title--tall"
-        style={{ gridArea: "1 / 1", color: "var(--color-white" }}
-      >
-        {txt}
-      </p>
-    </div>
-  </>;
+  return (
+    <>
+      <div className={classes} style={{ display: "grid" }}>
+        <p
+          className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-base title title--tall"
+          style={{
+            gridArea: "1 / 1",
+            color: "var(--color-black)",
+            animationDelay: "-0.1s",
+          }}
+        >
+          {txt}
+        </p>
+        <p
+          className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-layer-1 title title--tall"
+          style={{
+            gridArea: "1 / 1",
+            color: "var(--color-title)",
+            animationDelay: "-0.1s",
+          }}
+        >
+          {txt}
+        </p>
+        <p
+          className="h1 label-accent note wipe-down-on-scroll-0 no-delay gothic-layer-2 title title--tall"
+          style={{ gridArea: "1 / 1", color: "var(--color-white" }}
+        >
+          {txt}
+        </p>
+      </div>
+    </>
+  );
 };
 
 function Works() {
@@ -57,7 +59,6 @@ function Works() {
   useImageSizer();
   useScrollReveal();
   useSectionThemes();
-
 
   const sketch = createSketch("bg");
 
@@ -138,178 +139,179 @@ function Works() {
               </section>
 
               <section data-theme="dark-mode">
+                {GothicTitle(
+                  "Our work has been installed in public galleries, exhibitions, and open-air sites.",
+                  "dark-mode"
+                )}
 
-              {GothicTitle(
-                "Our work has been installed in public galleries, exhibitions, and open-air sites.",
-                "dark-mode"
-              )}
+                <div className="project-grid project-grid--full-w-24">
+                  <Tile w={6} h={6} title="120 Broadway">
+                    <ProjectHoverLink
+                      href="work/2019-broadway-mural-project"
+                      title="Broadway Mural Project"
+                    >
+                      <img
+                        data-src="/works/2019-client-mural-broadway/wide-still"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-              <div className="project-grid project-grid--full-w-24">
-                <Tile w={6} h={6} title="120 Broadway">
-                  <ProjectHoverLink
-                    href="work/2019-broadway-mural-project"
-                    title="Broadway Mural Project"
-                  >
-                    <img
-                      data-src="/works/2019-client-mural-broadway/wide-still"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-
-                <Tile w={6} h={6} title="World Trade Center">
-                  <ProjectHoverLink
-                    href="work/world-trade-center"
-                    title="World Trade Center"
-                  >
-                    <img data-src="/works/wtc/cover" data-fmt="jpg"></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={5} title="Map of the U.S.">
-                  <ProjectHoverLink
-                    href="work/us-map"
-                    title="Map of the United States"
-                  >
-                    <img
-                      data-src="/works/us-map/us-map-cover"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={5} title="London Review of Books">
-                  <ProjectHoverLink
-                    href="work/london-review-of-books"
-                    title="London Review of Books"
-                  >
-                    <img
-                      data-src="/works/london-review-of-books/cover"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={5} title="SFPC - Comms">
-                  <ProjectHoverLink
-                    href="work/sfpc-communication"
-                    title="Communication"
-                  >
-                    <img
-                      data-src="/works/sfpc-communication/cover"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-              </div>
+                  <Tile w={6} h={6} title="World Trade Center">
+                    <ProjectHoverLink
+                      href="work/world-trade-center"
+                      title="World Trade Center"
+                    >
+                      <img data-src="/works/wtc/cover" data-fmt="jpg"></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                  <Tile w={4} h={5} title="Map of the U.S.">
+                    <ProjectHoverLink
+                      href="work/us-map"
+                      title="Map of the United States"
+                    >
+                      <img
+                        data-src="/works/us-map/us-map-cover"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                  <Tile w={4} h={5} title="London Review of Books">
+                    <ProjectHoverLink
+                      href="work/london-review-of-books"
+                      title="London Review of Books"
+                    >
+                      <img
+                        data-src="/works/london-review-of-books/cover"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                  <Tile w={4} h={5} title="SFPC - Comms">
+                    <ProjectHoverLink
+                      href="work/sfpc-communication"
+                      title="Communication"
+                    >
+                      <img
+                        data-src="/works/sfpc-communication/cover"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                </div>
               </section>
               <section data-theme="theme-light">
+                {GothicTitle(
+                  "We design custom typefaces and lettering to fit every occasion."
+                )}
 
-              {GothicTitle(
-                "We design custom typefaces and lettering to fit every occasion."
-              )}
+                <div className="project-grid project-grid--full-w-24">
+                  <Tile w={6} h={8} title="Quick Brown Fox">
+                    <ProjectHoverLink
+                      href="work/quick-brown-fox"
+                      title="Quick Brown Fox"
+                    >
+                      <img
+                        data-src="/works/quick-brown-fox/cover"
+                        data-fmt="png"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-              <div className="project-grid project-grid--full-w-24">
-                <Tile w={6} h={8} title="Quick Brown Fox">
-                  <ProjectHoverLink
-                    href="work/quick-brown-fox"
-                    title="Quick Brown Fox"
-                  >
-                    <img
-                      data-src="/works/quick-brown-fox/cover"
-                      data-fmt="png"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
+                  <Tile w={6} h={8} title="Allida">
+                    <ProjectHoverLink href="work/allida-imprint" title="Allida">
+                      <img
+                        data-src="/works/2022-allida/cover"
+                        data-fmt="png"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                  <Tile w={4} h={11} title="Constant Typeface">
+                    <ProjectHoverLink
+                      href="work/constant-typeface"
+                      title="Constant Typeface"
+                    >
+                      <img
+                        data-src="/works/constant-typeface/constant-poster"
+                        data-fmt="jpeg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-                <Tile w={6} h={8} title="Allida">
-                  <ProjectHoverLink href="work/allida-imprint" title="Allida">
-                    <img
-                      data-src="/works/2022-allida/cover"
-                      data-fmt="png"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={4} title="Hany's Fire Tonic">
-                  <ProjectHoverLink
-                    href="work/hanys-fire-tonic"
-                    title="Hanys Fire Tonic"
-                  >
-                    <img
-                      data-src="/works/2018-client-brand-hanys-harvest/mockup_wide"
-                      data-fmt="png"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
+                  <Tile w={4} h={4} title="Hamilton Type — Etta">
+                    <ProjectHoverLink
+                      href="work/hamilton-etta"
+                      title="Hamilton Type - Etta"
+                    >
+                      <img
+                        data-src="/works/etta/Etta_Banners_01"
+                        data-fmt="png"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-                <Tile w={4} h={4} title="Hamilton Type — Etta">
-                  <ProjectHoverLink
-                    href="work/hamilton-etta"
-                    title="Hamilton Type - Etta"
-                  >
-                    <img
-                      data-src="/works/etta/Etta_Banners_01"
-                      data-fmt="png"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={11} title="Constant Typeface">
-                  <ProjectHoverLink
-                    href="work/constant-typeface"
-                    title="Constant Typeface"
-                  >
-                    <img
-                      data-src="/works/constant-typeface/constant-poster"
-                      data-fmt="jpeg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={9} title="7 Years of SFPC">
-                  <ProjectHoverLink
-                    href="work/sfpc-seven-years"
-                    title="7 Years of SFPC"
-                  >
-                    <img
-                      data-src="/works/2019-sfpc-poster/cover"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-                <Tile w={4} h={7} title="Chance and Destiny" classNames="">
-                  <ProjectHoverLink
-                    href="work/chance-and-destiny"
-                    title="Chance and Destiny"
-                  >
-                    <img
-                      data-src="/works/chance-and-destiny/Textura_process"
-                      data-fmt="jpg"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
+                  <Tile w={4} h={4} title="Hany's Fire Tonic">
+                    <ProjectHoverLink
+                      href="work/hanys-fire-tonic"
+                      title="Hanys Fire Tonic"
+                    >
+                      <img
+                        data-src="/works/2018-client-brand-hanys-harvest/mockup_wide"
+                        data-fmt="png"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-                <div className="tile rounded tile-w-4 tile-h-8 hide-sm wipe-down bg-diag-1"></div>
-                <Tile w={4} h={8} title="The Winter Garden">
-                  <ProjectHoverLink
-                    href="work/the-winter-garden"
-                    title="The Winter Garden"
-                  >
-                    <img
-                      data-src="/works/2022-winter-garden-cover/mockup"
-                      data-fmt="png"
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
+                  <Tile w={4} h={9} title="7 Years of SFPC">
+                    <ProjectHoverLink
+                      href="work/sfpc-seven-years"
+                      title="7 Years of SFPC"
+                    >
+                      <img
+                        data-src="/works/2019-sfpc-poster/cover"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
 
-                <Tile w={4} h={6} title="Kiplingers Magazine">
-                  <ProjectHoverLink
-                    href="work/kiplingers"
-                    title="Kiplingers Magazine"
-                  >
-                    <img
-                      data-src="/works/kiplingers-cover"
-                      data-fmt="jpg"
-                      style={{ transform: "scale(110%)" }}
-                    ></img>
-                  </ProjectHoverLink>
-                </Tile>
-              </div>
+                  <Tile w={4} h={5} title="Chance and Destiny" classNames="">
+                    <ProjectHoverLink
+                      href="work/chance-and-destiny"
+                      title="Chance and Destiny"
+                    >
+                      <img
+                        data-src="/works/chance-and-destiny/Chance_Destiny_Textura_Cutout_lr"
+                        data-fmt="jpg"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+
+                  <Tile w={4} h={8} title="The Winter Garden">
+                    <ProjectHoverLink
+                      href="work/the-winter-garden"
+                      title="The Winter Garden"
+                    >
+                      <img
+                        data-src="/works/2022-winter-garden-cover/mockup"
+                        data-fmt="png"
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+
+                  <Tile w={4} h={6} title="Kiplingers Magazine">
+                    <ProjectHoverLink
+                      href="work/kiplingers"
+                      title="Kiplingers Magazine"
+                    >
+                      <img
+                        data-src="/works/kiplingers-cover"
+                        data-fmt="jpg"
+                        style={{ transform: "scale(110%)" }}
+                      ></img>
+                    </ProjectHoverLink>
+                  </Tile>
+                  <div className="tile rounded tile-w-4 tile-h-4 hide-sm wipe-down bg-diag-1"></div>
+                </div>
               </section>
             </div>
           </div>
