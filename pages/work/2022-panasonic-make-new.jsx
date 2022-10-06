d472c945274a890164c7390469782cbfe998c52e
Kevin Yeh <kevinyeah@utexas.edu>
2022-10-06 10:54:30 -0400

    update content

--- pages/work/2022-panasonic-make-new.jsx
+++ pages/work/2022-panasonic-make-new.jsx
@@ -8,10 +8,9 @@ export default function MakeNew() {
       intro={
         <>
           <p>
-            For Panasonic's new campaign, Space Type was invited alongside other artists to
-            share our own interpretations of their new slogan, "Make New." The resulting 30-second advert
-            was aired on Japanese TV channels and compiled into a website showcasing individual
-            works.
+            Space Type was invited alongside other artists to share our own interpretations of Panasonic's 
+            new slogan, "Make New." The resulting 30-second advert was aired on Japanese TV channels and 
+            compiled into a website showcasing individual works.
           </p>
           <p>All motion graphics were built for the web using p5.js and custom shaders.</p>
         </>
