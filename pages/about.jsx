7940ca1fb4a418049507e3effbd870c1b50deacc
Kevin Yeh <kevinyeah@utexas.edu>
2023-10-06 16:58:19 -0400

    big lists of stuff

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,6 +7,542 @@ function About() {
   useLogo();
   useImageSizer();
 
+  const TEACHING_AND_WORKSHOPS = [
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "Currents: CD Workshop (Machine Learning)",
+      dates: "Oct-Nov 2023",
+    },
+    {
+      host: "Type Electives",
+      link: "",
+      title: "Pixel Type Workshop",
+      dates: "Mar 2023",
+    },
+    {
+      host: "Type Electives",
+      link: "",
+      title: "Intro to Generative Typography with p5.js",
+      dates: "Jan-Apr 2023",
+    },
+    {
+      host: "Letràstrica",
+      link: "",
+      title: "Generative Typography",
+      dates: "Oct 2023",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Type West: Type Design",
+      dates: "Jun-Aug 2022",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Prototyping Letterforms",
+      dates: "Jun 2022",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Designing Flourishes",
+      dates: "Jun 2022",
+    },
+    {
+      host: "Typographics",
+      link: "",
+      title: "Code x Kinetic Type",
+      dates: "Jun 2022",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Generative Typography",
+      dates: "Feb-Apr 2022",
+    },
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "Type Design",
+      dates: "Jan-May 2022",
+    },
+    {
+      host: "Typographics",
+      link: "",
+      title: "Code x Typography Series",
+      dates: "Jun 2021",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "The History and Anatomy of Flourishing",
+      dates: "Mar 2021",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Type West: Type Design",
+      dates: "Jun-Aug 2021",
+    },
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "Type Design",
+      dates: "Jan-May 2021",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Generative Typography",
+      dates: "Oct-Dec 2020",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Generative Typography",
+      dates: "Jun-Jul 2020",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Intro to Lettering",
+      dates: "Jun 2020",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Hand Lettering",
+      dates: "Feb-Apr 2020",
+    },
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "Type Design",
+      dates: "2020",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Type West Core Workshop",
+      dates: "2020",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "Unconventional Tools",
+      dates: "2019",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Calligraphy: Italic and Bâtarde",
+      dates: "2019",
+    },
+    {
+      host: "Ladies Wine & Design",
+      link: "",
+      title: "Type Design 101",
+      dates: "2019",
+    },
+    {
+      host: "SVA Residency in Typography",
+      link: "",
+      title: "Calligraphy for Type Design",
+      dates: "2019",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Calligraphy: Historical Hands",
+      dates: "2019",
+    },
+    {
+      host: "Letteform Archive",
+      link: "",
+      title: "Calligraphy as a Tool for Prototyping Letterforms",
+      dates: "2019",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Designing Flourishes",
+      dates: "2019",
+    },
+    {
+      host: "ECV Paris",
+      link: "",
+      title: "Masterclass",
+      dates: "2019",
+    },
+    {
+      host: "Type@Cooper",
+      link: "",
+      title: "Calligraphy: Blackletter Fundamentals",
+      dates: "2019",
+    },
+    {
+      host: "HUGE Inc",
+      link: "",
+      title: "Blackletter Calligraphy",
+      dates: "2018",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "Basics of Blackletter",
+      dates: "2017",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "Flourishing Design",
+      dates: "2017",
+    },
+    {
+      host: "TypeCon",
+      link: "",
+      title: "Flourishing Design",
+      dates: "2017",
+    },
+    {
+      host: "Cooper Union",
+      link: "",
+      title: "Calligraphy (Guest Instructor)",
+      dates: "2017",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "Writing with Gradients",
+      dates: "2017",
+    },
+  ];
+
+  const teachingSplitIndex = Math.ceil(TEACHING_AND_WORKSHOPS.length / 2);
+
+  const SPEAKING_ENGAGEMENTS = [
+    {
+      host: "MIT Media Lab",
+      link: "",
+      title: "Future Sketches Lunch Lectures",
+      dates: "Sep 2023",
+    },
+    {
+      host: "ATypI Paris",
+      link: "",
+      title: "Designing with Imperfect Machines",
+      dates: "May 2023",
+    },
+    {
+      host: "MassArt",
+      link: "",
+      title: "Communication Design Lecture Series",
+      dates: "May 2023",
+    },
+    {
+      host: "Letràstrica",
+      link: "",
+      title: "Perfection Doesn't Exist",
+      dates: "Oct 2022",
+    },
+    {
+      host: "Typographics",
+      link: "",
+      title: "Main Conference",
+      dates: "Jun 2022",
+    },
+    {
+      host: "Typographics TypeLab",
+      link: "",
+      title: "Vartype: Generative sketches for variable fonts",
+      dates: "Jun 2022",
+    },
+    {
+      host: "GRANSHAN",
+      link: "",
+      title: "Sign of the Times",
+      dates: "May 2022",
+    },
+    {
+      host: "Type Network",
+      link: "",
+      title: "Designing Tomorrow Today",
+      dates: "Apr 2022",
+    },
+    {
+      host: "Society of Scribes",
+      link: "",
+      title: "History and Anotomy of Flourishing",
+      dates: "Mar 2021",
+    },
+    {
+      host: "ATypI",
+      link: "",
+      title: "Type, Technology, and Beyond",
+      dates: "Oct 2020",
+    },
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "CD Lecture Series",
+      dates: "Sep 2020",
+    },
+    {
+      host: "Parsons School of Design",
+      link: "",
+      title: "'History of the Latin Alphabet' Lecture Series",
+      dates: "Sep 2020",
+    },
+    {
+      host: "Typographics TypeLab",
+      link: "",
+      title: "Type, Technology, & Algorithms",
+      dates: "Sep 2020",
+    },
+    {
+      host: "Wayzgoose",
+      link: "",
+      title: "Material Creativity: Finding Inspiration in New Mediums",
+      dates: "2019",
+    },
+    {
+      host: "TypeCon",
+      link: "",
+      title: "History and Anatomy of Flourishing",
+      dates: "2019",
+    },
+    {
+      host: "Typographics TypeLab",
+      link: "",
+      title: "Behind the History of the Type Studio",
+      dates: "2019",
+    },
+    {
+      host: "Letterform Archive",
+      link: "",
+      title: "Letters: A Code System",
+      dates: "2019",
+    },
+    {
+      host: "TypeCon",
+      link: "",
+      title: "Reviving a Calligraphy Hand",
+      dates: "2018",
+    },
+    {
+      host: "Typographics TypeLab",
+      link: "",
+      title: "History of Flourishing",
+      dates: "2017",
+    },
+    {
+      host: "HUGE Inc",
+      link: "",
+      title: "Type Journey",
+      dates: "2017",
+    },
+    {
+      host: "Frog Design",
+      link: "",
+      title: "Creative Forum",
+      dates: "2017",
+    },
+    {
+      host: "Typographics: The Magazine",
+      link: "",
+      title: "Failures",
+      dates: "2016",
+    },
+  ];
+
+  const INTERVIEWS_AND_PRESS = [
+    {
+      host: "OHno Radio",
+      link: "",
+      dates: "2023",
+    },
+    {
+      host: "I Love Typography (ILT)",
+      link: "",
+      dates: "2021",
+    },
+    {
+      host: "Night Snack Club",
+      link: "",
+      dates: "2020",
+    },
+    {
+      host: "League of Movable Type Podcast",
+      link: "",
+      dates: "2020",
+    },
+    {
+      host: "Debugger",
+      link: "",
+      title: "Crowdfunding an Online Course During Quarantine",
+      dates: "2020",
+    },
+    {
+      host: "NaN",
+      link: "",
+      title: "Glyphzian Space",
+      dates: "2020",
+    },
+    {
+      host: "Letter Arts Review Vol. 33:4",
+      link: "",
+      dates: "2020",
+    },
+    {
+      host: "ECV NY Masterclass",
+      link: "",
+      dates: "2019",
+    },
+    {
+      host: "Communication Arts",
+      link: "",
+      dates: "2019",
+    },
+    {
+      host: "Alphabet",
+      link: "",
+      dates: "2017",
+    },
+    {
+      host: "Print Magazine",
+      link: "",
+      dates: "2017",
+    },
+    {
+      host: "Typodarium",
+      link: "",
+      dates: "2017",
+    },
+    {
+      host: "Letter Arts Review Vol. 31",
+      link: "",
+      dates: "2017",
+    },
+    {
+      host: "TypeThursday",
+      link: "",
+      dates: "2016",
+    },
+    {
+      host: "Art Directors Club",
+      link: "",
+      dates: "2016",
+    },
+    {
+      host: "Working Not Working",
+      link: "",
+      dates: "2015",
+    },
+    {
+      host: "Campaign US",
+      link: "",
+      dates: "2015",
+    },
+    {
+      host: "KERA (NPR & PBS)",
+      link: "",
+      dates: "2015",
+    },
+    {
+      host: "CMYK Magazine #7",
+      link: "",
+      dates: "2015",
+    },
+    {
+      host: "CA Korea Magazine #183",
+      link: "",
+      dates: "2015",
+    },
+  ];
+
+  const EXHIBITIONS = [
+    {
+      host: "Schick Art Gallery",
+      link: "",
+      title: "True to Type",
+      dates: "2022",
+    },
+    {
+      host: "Letràstrica",
+      link: "",
+      dates: "2022",
+    },
+    {
+      host: "School for Poetic Computation",
+      link: "",
+      title: "Poetic Computation: Seven Years of SFPC",
+      dates: "2020",
+    },
+    {
+      host: "Found in Translation",
+      link: "",
+      dates: "2018",
+    },
+    {
+      host: "Type Directors Club",
+      link: "",
+      title: "Ascenders",
+      dates: "2018",
+    },
+    {
+      host: "The Grolier Club",
+      link: "",
+      title: "The Calligraphy Revival",
+      dates: "2017",
+    },
+  ];
+
+  const PUBLICATION_FAIRS = [
+    {
+      host: "Hester Street Fair",
+      link: "",
+      dates: "Sep 2023",
+    },
+    {
+      host: "Flamecon",
+      link: "",
+      dates: "Aug 2023",
+    },
+    {
+      host: "Typographics Book Fair",
+      link: "",
+      dates: "Jun 2023",
+    },
+    {
+      host: "Principles Vendor Market",
+      link: "",
+      dates: "Jun 2023",
+    },
+    {
+      host: "Other Islands Book Fair",
+      link: "",
+      dates: "Apr 2023",
+    },
+  ];
+
+  const Table = (items) => (
+    <table className="table-auto col-span-6 min-w-full">
+      <tbody>
+        {items.map((info) => (
+          <tr>
+            <td>
+              <a href={info.link}>{info.host}</a>
+            </td>
+            <td>{info.title}</td>
+            <td>{info.dates}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
   return (
     <>
       <Head>
@@ -28,10 +564,26 @@ function About() {
         a,
         span,
         ul,
-        input {
+        input,
+        td {
           font-family: "ST Monochromic", monospace;
           font-weight: 60;
-          font-size: 0.8em;
+          font-size: 0.8rem;
+        }
+
+        td {
+          padding-block: 4px;
+          border-bottom: 1px solid #333;
+        }
+
+        td:last-child {
+          text-align: right;
+        }
+
+        td:not(:last-child),
+        td a {
+          padding-right: 5px;
+          font-size: 0.7rem;
         }
 
         #nav-area a,
@@ -42,6 +594,11 @@ function About() {
         .footer input {
           font-family: "AdelleMonoFlex-Regular", monospace;
           font-size: reset;
+        }
+
+        .grid {
+          max-width: none;
+          margin: none;
         }
       `}</style>
       <main>
@@ -135,7 +692,7 @@ function About() {
               </div>
             </section>
 
-            <section className="content max-w-7xl">
+            <section className="content-wide max-w-7xl">
               <h1 className="text-3xl">Our Capabilities</h1>
               <div>
                 <div className="max-w-2xl">
@@ -152,7 +709,7 @@ function About() {
                   </p>
                 </div>
               </div>
-              <div>
+              <div className="max-w-2xl">
                 <p className="underline">
                   Interactive Digital Systems and Tools
                 </p>
@@ -215,6 +772,66 @@ function About() {
                     <div className="small-link">Vartype →</div>
                   </a>
                 </Link>
+              </div>
+            </section>
+
+            <div className="flex flex-row justify-center mb-24">
+              <hr className="w-48 opacity-50" />
+            </div>
+
+            <section className="content-wide">
+              <h1 className="text-3xl">Teaching and Workshops</h1>
+              <div className="grid gap-6">
+                {[0, 1].map((tableNum) =>
+                  Table(
+                    TEACHING_AND_WORKSHOPS.slice(
+                      tableNum * teachingSplitIndex,
+                      (tableNum + 1) * teachingSplitIndex
+                    )
+                  )
+                )}
+              </div>
+              <div className="grid">
+                <div className="col-span-6">
+                  <h1 className="text-3xl">Select Speaking Engagements</h1>
+                  {Table(SPEAKING_ENGAGEMENTS)}
+                </div>{" "}
+                <div className="col-span-6">
+                  <h1 className="text-3xl">Interviews and Press</h1>
+                  {Table(INTERVIEWS_AND_PRESS)}
+                </div>
+              </div>
+              <div className="flex flex-row items-start justify-start gap-x-2.5 max-h-lg my-12">
+                <img
+                  src="/img/about-new/speaking.png"
+                  className="object-contain h-96"
+                />
+                {/* TODO: convert HDR image so it shows correctly */}
+                <img
+                  src="/img/about-new/exhibition.png"
+                  className="object-contain h-96"
+                />
+              </div>
+              <div className="max-w-lg">
+                <h1 className="text-3xl">Select Exhibitions</h1>
+                {Table(EXHIBITIONS)}
+              </div>
+              <div className="flex flex-row items-end justify-end gap-x-2.5 max-h-lg min-w-full my-12">
+                <img
+                  src="/img/about-new/zine-table.png"
+                  className="object-contain h-96"
+                />
+                {/* TODO: convert HDR image so it shows correctly */}
+                <img
+                  src="/img/about-new/zine-hester-st-fair.png"
+                  className="object-contain h-96"
+                />
+              </div>
+              <div className="flex flex-row justify-end">
+                <div className="max-w-lg" style={{ minWidth: "32rem" }}>
+                  <h1 className="text-3xl">Publication Fairs</h1>
+                  {Table(PUBLICATION_FAIRS)}
+                </div>
               </div>
             </section>
 
